1910c8c93a79f15a35c44ee48292b5470a918311
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2025-11-27 13:53:48 -0300

    Add Aula02

---
+++ Aula02/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Aula02;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Olá Mundo");
+        //Comentário
+        /**
+        Váriaveis Simples
+        **/
+
+        //Variavel para Nome
+        string myName = "Nicolas";
+
+        Console.WriteLine(myName);
+
+        //Variavel para Número
+        int number = 10;
+
+        Console.WriteLine(number);
+
+        //Variavel para número com virgula
+        float numberFloat = 5.2;
+        
+        Console.WriteLine(numberFloat);
+
+        //Variavel para caracteres
+        char character ='t';
+
+        Console.WriteLine(character);
+
+        //Variavel para teste Lódigo(verdade ou falso)
+        bool teste = true;
+
+        Console.WriteLine(teste);
+
+    }
+}
