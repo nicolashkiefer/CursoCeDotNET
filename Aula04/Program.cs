83119ff593659088440446d566b673e7c8a0dce8
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2025-11-27 16:52:35 -0300

    Add Aula04

---
+++ Aula04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula04;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Output de dados
+        Console.WriteLine("Saída!");
+
+        Console.WriteLine("--------Cadastro de Usuário--------");
+
+        //Solicitar o nome
+        Console.WriteLine("Digite seu nome: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Digite sua idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("-----------------------------------");
+
+        Console.WriteLine("Nome do Usuário: "+ name);
+        Console.WriteLine("Idade do Usuário: "+ age);
+
+    }
+}
