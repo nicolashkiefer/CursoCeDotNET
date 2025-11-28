cf06b478ad568dd0f579b6cc2fcc74c2a68f25e9
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2025-11-28 13:18:38 -0300

    add Aula05

---
+++ Aula05/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Aula05
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Operadores Aritméticos
+
+            Console.WriteLine("=======Calculadora=======");
+            Console.WriteLine("Digite o primeiro valor: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+           //Adição
+            int sum = number1 + number2;
+            Console.WriteLine("O valor da soma é "+sum);
+
+            //Subtração
+            int subtraction = number2 - number1;
+            Console.WriteLine("O valor da subtração é "+subtraction);
+
+            //Multiplicação
+            int multiplication = number1 * number2;
+            Console.WriteLine("O valor da multiplicação é "+multiplication);
+
+            //Divição
+            int division = number2 / number1;
+            Console.WriteLine("O valor da divisão é "+division);
+
+            //Modulo(Resto da divisão)
+            int module = number1 % number2;
+            Console.WriteLine("O valor do resto da divisão é "+module);
+
+        }    
+    }
+}
