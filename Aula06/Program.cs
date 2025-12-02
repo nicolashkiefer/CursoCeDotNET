445e5d6f7499de02ae83e8ff6394719a223490c4
Nícolas Henrique Kiefer <nicolashkiefer@gmail.com>
2025-12-02 15:05:44 -0300

    Add Aula06

---
+++ Aula06/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Aula06
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Estrutura Condicional: If-Else
+
+            Console.WriteLine("Digite a sua Idade: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age > 18)
+            {
+                Console.WriteLine("Você é maior de Idade!");
+            }else
+                Console.WriteLine("Você é menor de Idade!");
+
+            Console.WriteLine("=======Verificador de Par e Ímpares=======");
+            Console.WriteLine("Digite um numero: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if(number%2 == 0)
+            {
+                Console.WriteLine("O número é PAR!");
+            }else
+                Console.WriteLine("O Número é ÍMPAR!");
+
+        }
+    }
+}
